0c78b78018345baabd4b034abf942377de0e82f5
Pushan <mukhopadhyaypushan42@gmail.com>
2025-06-12 19:11:54 +0530

    final commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async ()=>{
     try{
-        // await mongoose.connect(`${process.env.DB_URL}/thinkboard`)
-        await mongoose.connect(`${process.env.DB_URL}`)
+        await mongoose.connect(`${process.env.DB_URL}/thinkboard`)
         console.log("Connected to the database successfully");
     }catch(err){
         console.log("Error in connecting to the database", err.message)
