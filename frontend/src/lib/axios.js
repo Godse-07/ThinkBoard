8a2998a005acb7bfdedcbbd5112c2f478fc5f1e0
Pushan <mukhopadhyaypushan42@gmail.com>
2025-06-13 19:16:49 +0530

    db update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://thinkboard-2g8j.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
